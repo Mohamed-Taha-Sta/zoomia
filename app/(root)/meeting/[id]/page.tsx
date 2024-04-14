fce23a4bfc3e67e6d3ab36f6bf74a73c0e715d71
Taha <mohamedtahasta@gmail.com>
2024-04-14 23:35:48 +0100

    Changed README.md

---
+++ app/(root)/meeting/[id]/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Page = ({params}: {params: {id: string}}) => {
+    return (
+        <div>
+            Meeting number #{params.id}
+        </div>
+    );
+};
+
+export default Page;
