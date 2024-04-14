7c3ec3654fe08c90216760b524ae4f832698de51
Taha <mohamedtahasta@gmail.com>
2024-04-15 00:04:04 +0100

    Added dark 1 & dark 2 to tailwind.config.ts, added layout files for app & home and made the Navbar.tsx and Sidebar.tsx files/

---
+++ app/(root)/(home)/layout.tsx
@@ -0,0 +1,21 @@
+import React, {ReactNode} from 'react';
+import Navbar  from '@/components/Navbar';
+import Sidebar  from '@/components/Sidebar';
+
+const HomeLayout = ({children}: { children: ReactNode }) => {
+    return (
+        <div className={"relative"}>
+            <Navbar/>
+            <div className={"flex"}>
+                <Sidebar/>
+                <section className={"flex min-h-screen flex-1 flex-col px-6 pb-6 pt-28 max-md:pb-14 sm:px-14"}>
+                    <div className={"w-full"}>
+                        {children}
+                    </div>
+                </section>
+            </div>
+        </div>
+    );
+};
+
+export default HomeLayout;
