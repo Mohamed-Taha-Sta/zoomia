c78a08a84999db46f7c5f0a79c13bb8cf9980a43
Taha <mohamedtahasta@gmail.com>
2024-05-24 22:36:59 +0100

    Upcoming meeting window is now dynamic.

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,6 +1,8 @@
 "use client";
 import React, {useEffect, useState} from 'react';
 import MeetingTypeList from "@/components/MeetingTypeList";
+import {useGetCalls} from "@/hooks/useGetCalls";
+import {Call} from "@stream-io/video-react-sdk";
 
 const Page = () => {
     const [shownTime, setShownTime] = useState(() => new Date().toLocaleTimeString('en-US', {
@@ -18,13 +20,20 @@ const Page = () => {
             clearInterval(interval);
         };
     }, []);
+    const {upcomingCalls} = useGetCalls();
 
+    let upcomingMsg = "Upcoming Meeting on: ";
+
+    if(!upcomingCalls || upcomingCalls.length == 0) upcomingMsg = "No Upcomming Meetings";
+    else {
+        upcomingMsg += `${(upcomingCalls[0] as Call).state?.startsAt?.toLocaleString()}`;
+    };
     return (
         <section className={"flex size-full flex-col gap-10 text-white"}>
             <div className={"h-[300px] w-full rounded-[20px] bg-hero bg-cover"}>
                 <div className={"flex h-full flex-col justify-between px-5 py-8 lg:p-11"}>
-                    <h2 className={"glassmorphism max-w-[270px] rounded py-2 text-base font-normal text-center"}>
-                        Upcoming Meeting at: 12:09 PM
+                    <h2 className={"glassmorphism max-w-[400px] rounded py-2 text-base font-normal text-center"}>
+                        {upcomingMsg}
                     </h2>
                     <div className={"flex flex-col gap-2"}>
                         <h1 className={"text-4xl font-extrabold lg:text-7xl"}>
